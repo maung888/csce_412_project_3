9776f2ff4116e9f54385d76d9157132ae0d480c8
maung888 <123016436+maung888@users.noreply.github.com>
2024-06-15 23:24:25 -0500

    main.cpp added

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+int main() {
+    int numServers, duration;
+    std::cout << "Enter the number of servers: ";
+    std::cin >> numServers;
+    std::cout << "Enter the duration to run the load balancer: ";
+    std::cin >> duration;
+
+    LoadBalancer lb(numServers);
+    lb.run(duration);
+
+    return 0;
+}
